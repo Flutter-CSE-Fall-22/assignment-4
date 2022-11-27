c0ee7e0466968c2e43eb8e9b07a45a1e3547e1c0
vishalramiba <95579831+vishalramiba@users.noreply.github.com>
2022-11-27 21:40:11 +0500

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutterdemo/firebase_options.dart';
 import 'package:flutterdemo/views/add_book.dart';
 import 'package:flutterdemo/views/book_list.dart';
 import 'package:provider/provider.dart';
@@ -8,7 +9,7 @@ import 'controller/book_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(ChangeNotifierProvider(
     child: const MyApp(),
